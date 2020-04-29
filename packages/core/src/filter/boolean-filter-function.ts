a7df248225d9530f04174eebd166dad60ff9fb14
Marika Marszalkowski <marika.marszalkowski@sap.com>
2020-04-29 15:42:48 +0200

    fsdafg

--- packages/core/src/filter/boolean-filter-function.ts
+++ packages/core/src/filter/boolean-filter-function.ts
@@ -9,7 +9,7 @@ export class BooleanFilterFunction<
   EntityT extends Entity
 > extends FilterFunction<EntityT, boolean> {
   /**
-   * Creates an instance of BooleanFilterFunction.
+   * Creates an instance of BooleanFilterFunction. Go.
    * @param functionName - - Name of the function that returns a boolean value
    * @param parameters - - Representation of the parameters passed to the filter function
    */
